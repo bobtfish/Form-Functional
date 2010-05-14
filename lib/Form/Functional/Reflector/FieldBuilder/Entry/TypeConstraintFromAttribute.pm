4751ca486b02b90bf3f324143b4e7c0dcd4431ad
Tomas Doran <bobtfish@bobtfish.net>
2010-05-14 15:36:55 +0200

    Add type coercion handling, ergo fixing the tests

--- lib/Form/Functional/Reflector/FieldBuilder/Entry/TypeConstraintFromAttribute.pm
+++ lib/Form/Functional/Reflector/FieldBuilder/Entry/TypeConstraintFromAttribute.pm
@@ -8,7 +8,13 @@ method match { 1 }
 
 method apply ($result, $item) {
     my $tc = $item->has_type_constraint ? $item->type_constraint : Any;
-    $result->clone_and_set(type_constraints => [$tc]);
+    my $new_result = $result->clone_and_set(type_constraints => [$tc]);
+
+    if ($tc->has_coercion) {
+        $new_result = $new_result->clone_and_set(coerce => 1);
+    }
+
+    return $new_result;
 }
 
 with 'Form::Functional::Reflector::FieldBuilder::Entry';
