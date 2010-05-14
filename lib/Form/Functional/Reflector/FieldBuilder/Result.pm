ac0d5419dde550d53fe0cc79fd9c84ae05f252b5
Tomas Doran <bobtfish@bobtfish.net>
2010-05-14 22:12:10 +0200

    More recursive and add merge methods.

--- lib/Form/Functional/Reflector/FieldBuilder/Result.pm
+++ lib/Form/Functional/Reflector/FieldBuilder/Result.pm
@@ -2,6 +2,8 @@ package Form::Functional::Reflector::FieldBuilder::Result;
 use Moose;
 use Method::Signatures::Simple;
 use MooseX::Types::Moose qw/ HashRef /;
+use Carp qw/croak/;
+use Hash::Merge 0.12;
 use namespace::autoclean;
 
 with 'MooseX::Clone';
@@ -16,7 +18,34 @@ has data => (
     },
 );
 
+around data => sub {
+    my ($orig, $self) = @_;
+    my %data = $self->$orig;
+    #use Data::Dumper;
+    #warn Dumper \%data;
+    foreach my $k (keys %data) {
+        if (blessed($data{$k})||'' eq __PACKAGE__) {
+            $data{$k} = { $data{$k}->data };
+        }
+    }
+    return %data;
+};
+
+around get => sub {
+    my ($orig, $self, $key) = @_;
+    my $value = $self->$orig($key);
+    if (blessed($value)||'' eq __PACKAGE__) {
+        return { $value->data };
+    }
+    return $value;
+};
+
 method clone_and_set ($key, $value) {
+    if (ref($value) eq 'HASH') {
+        $value = __PACKAGE__->new(data => $value);
+        croak "Cannot merge data in $key with pre-existing data without a merge direction - try clone_and_merge_r or clone_and_merge_l"
+            if (ref($self->get($key)) eq 'HASH');
+    }
     $self->clone(data => { $self->data, $key => $value });
 }
 
@@ -25,6 +54,29 @@ method clone_and_delete ($key) {
     delete $data{$key};
     $self->clone(data => \%data);
 }
+
+method _clone_and_merge ($merger, $key, $val) {
+    my $current = $self->get($key);
+    my $type = ref($current) || 'SCALAR';
+    croak "Cannot merge hash with non hash ($type) for key $key"
+        unless $type eq 'HASH';
+    #use Devel::Dwarn;
+    #Dwarn [$current, $val];
+    $self->clone_and_delete($key)->clone_and_set($key, $merger->merge($current, $val));
+}
+
+my $lm = Hash::Merge->new( 'LEFT_PRECEDENT' );
+method clone_and_merge_l ($key, $val) {
+    $self->_clone_and_merge($lm, $key, $val);
+}
+
+my $rm = Hash::Merge->new( 'RIGHT_PRECEDENT' );
+method clone_and_merge_r ($key, $val) {
+    $self->_clone_and_merge($rm, $key, $val);
+}
+
+#clone_merge_{l,r}(@path, $val)
+#clone_with(@kv_pairs) and clone_without(@k_list)
 
 __PACKAGE__->meta->make_immutable;
 1;
--- t/reflector_fieldbuilder_result.t
+++ t/reflector_fieldbuilder_result.t
@@ -2,6 +2,7 @@ use strict;
 use warnings;
 
 use Test::More;
+use Test::Exception;
 
 use aliased 'Form::Functional::Reflector::FieldBuilder::Result';
 
@@ -10,11 +11,28 @@ is scalar($r1->data), 0;
 
 my $r2 = $r1->clone_and_set(foo => 'bar');
 is_deeply {$r2->data}, {foo => 'bar'};
+is $r2->get('foo'), 'bar';
 isnt $r1, $r2;
 
 my $r3 = $r2->clone_and_delete('foo');
 is scalar($r3->data), 0;
 isnt $r3, $r2;
 isnt $r3, $r1;
+
+my $r4 = $r2->clone_and_set(baz => { some => 'data'});
+is_deeply {$r4->data}, { foo => 'bar', baz => { some => 'data'} };
+is_deeply $r4->get('baz'), {some => 'data'};
+
+throws_ok { $r4->clone_and_set( baz => { other => 'data'} ) }
+    qr/annot merge/;
+
+my $r5 = $r4->clone_and_merge_l( baz => { other => 'data' } );
+is_deeply {$r5->data}, { foo => 'bar', baz => { some => 'data', other => 'data'} };
+
+my $r6 = $r5->clone_and_merge_l( baz => { other => 'changed' } );
+is_deeply {$r6->data}, { foo => 'bar', baz => { some => 'data', other => 'data'} };
+
+my $r7 = $r6->clone_and_merge_r( baz => { other => 'changed' } );
+is_deeply {$r7->data}, { foo => 'bar', baz => { some => 'data', other => 'changed'} };
 
 done_testing;
