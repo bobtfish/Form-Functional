6479297486a4854aab77f766bf14834bd730c011
Tomas Doran <bobtfish@bobtfish.net>
2010-05-15 18:15:44 +0200

    Add a placeholder module for the main F::F

---
+++ lib/Form/Functional.pm
@@ -0,0 +1,20 @@
+package Form::Functional;
+use strict;
+use warnings;
+
+our $VERSION = '0.001';
+
+1;
+
+__END__
+
+=head1 SYNOPSIS
+
+    FIXME
+
+=head1 DESCRIPTION
+
+A forms system which actually works!
+
+=cut
+
