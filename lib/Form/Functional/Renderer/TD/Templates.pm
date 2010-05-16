41c1bdb035aa1ee7b7f1c6618e6c18405ec252da
Tomas Doran <bobtfish@bobtfish.net>
2010-05-16 12:27:26 +0200

    Start displaying some sort of field legends, and errors. Kinda.

--- lib/Form/Functional/Renderer/TD/Templates.pm
+++ lib/Form/Functional/Renderer/TD/Templates.pm
@@ -4,6 +4,7 @@ use warnings;
 use Method::Signatures::Simple;
 use Template::Declare::Tags;
 use Moose::Autobox 0.11;
+use Scalar::Util qw/refaddr/;
 use namespace::clean -except => [qw/ meta /];
 
 use base 'Template::Declare';
@@ -15,10 +16,11 @@ sub make_form (&) {
         my @fields = $params{form}->fields;
         my %extra;
         if ($params{render_data}) {
-            foreach my $name (qw/ method action /) {
+            foreach my $name (qw/ method action id class /) {
                 $extra{$name} = $params{render_data}->{$name} if $params{render_data}->{$name};
             }
         }
+        $extra{id} = "form_" . refaddr($params{form}) unless $extra{id};
 
         form {
             attr { %extra, %{ $params{attr} || {} } };
@@ -34,8 +36,16 @@ sub make_input (;&) {
     my ($cb) = @_;
     smart_tag_wrapper {
         my %params = @_;
+        my $field_id = 'form_XXXX_' . $params{name};
+        label {
+            attr {
+                for => $field_id
+            }
+            ucfirst $params{name};
+        }
         input {
             attr {
+                id => $field_id,
                 type => 'text',
                 name => $params{name},
                 $cb ? (value => $cb->()) : (),
@@ -70,17 +80,33 @@ template form => sub {
 
 template processed_field => sub {
     my ($self, $processed, $name, $field) = @_;
-    with (
-        name  => $name,
-        field => $field,
-    ), make_input {
-        ($processed->values_for($name))[0] # FIXME!
-            if $processed->values_exist_for($name);
-    };
+    span {
+        with (
+            name  => $name,
+            field => $field,
+        ), make_input {
+            ($processed->values_for($name))[0] # FIXME!
+                if $processed->values_exist_for($name);
+        };
+        if (my $errors = {$processed->errors}->{$name}) {
+            foreach my $error ($errors->flatten) {
+                div {
+                    attr {
+                        class => 'error'
+                    };
+                    $error->message;
+                };
+            };
+        }
+    }
 };
 
 template processed => sub {
     my ($self, $processed) = @_;
+
+#    use Devel::Dwarn;
+#    local $Data::Dumper::Maxdepth = 4;
+#    Dwarn $processed;
 
     my %data = (
         form => $processed,
